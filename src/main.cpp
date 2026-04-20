830ae7766e038c1044dd18cb7c1e9abb3efb9a1f
Kobe <243079596+k0bes@users.noreply.github.com>
2026-04-19 21:06:10 -0400

    Sync Repo

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,13 @@
 #include <boop/render/render.h>
 
 int main() {
-    Window window = Window("Example Window",600,400);
+    Window window = Window("Example Window",800,600);
     Renderer render = Renderer(window);
     
     while (window.isOpen()) {
         window.start();
 
-        render.rect(0,0,100,20,Color(255,255,255));
+        render.rect(0,0,100,80,Color(255,255,255));
 
         window.end();
     }
