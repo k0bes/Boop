77324758401a0d2fabffcacb5d96e9563f129ff7
Kobe <243079596+k0bes@users.noreply.github.com>
2026-04-19 20:19:57 -0400

    Implement Window.h

--- src/boop/window/window.h
+++ src/boop/window/window.h
@@ -13,8 +13,29 @@ class Window
 private:
     SDL_Window* window;
     SDL_Renderer* render;
+    SDL_Event event;
+
+    bool open;
 public:
     Window(string name, int width, int height);
+    bool isOpen() {
+        return open;
+    }
+
+    void start() {
+        SDL_SetRenderDrawColor(render, 0,0,0,0);
+        SDL_RenderClear(render);
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT)  {
+                open = false;
+            }
+
+        }
+    }
+
+    void end() {
+        SDL_RenderPresent(render);
+    }
 };
 
 Window::Window(string name, int width, int height)
@@ -41,5 +62,8 @@ Window::Window(string name, int width, int height)
         SDL_Quit();
         exit(-1);
     }
-    
+
+    open = true;
+
+
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <boop/window/window.h>
 int main() {
-    Window window = Window("text",600,400);
+    Window window = Window("Example Window",600,400);
+    while (window.isOpen()) {
+        window.start();
+        window.end();
+    }
     
 }
