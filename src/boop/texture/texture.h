57a78565a9a93c91ee21a0d270e449264ef83cf1
Kobe <243079596+k0bes@users.noreply.github.com>
2026-04-20 15:10:36 -0400

    Add extra parms to render function

--- src/boop/texture/texture.h
+++ src/boop/texture/texture.h
@@ -11,6 +11,18 @@ private:
     Window& win;
 public:
     Texture(Window& window, std::string textureLocation);
+
+    // void drawFill() {
+    //     SDL_RenderTexture(win._SDL_RENDERER(), texture, NULL, NULL);
+    // }
+
+    void render(float x, float y, float width, float height) {
+        SDL_FRect dst = {x, y, width, height};
+        SDL_RenderTexture(win._SDL_RENDERER(), texture, NULL, &dst);
+
+    }
+
+    
 };
 
 Texture::Texture(Window& window, string textureLocation): win(window)
--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,14 @@
 #include <boop/texture/texture.h>
 
 int main() {
-    Window window = Window("Example Window",800,600);
+    Window window = Window("Example Window",1280,720);
     Renderer render = Renderer(window);
     Texture texture = Texture(window, "image.png");
     
     while (window.isOpen()) {
         window.start();
-        render.rect(0,0,100,80,Color(255,255,255));
+
+        texture.render(0,0,693,199);
 
         window.end();
     }
