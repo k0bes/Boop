a8ba76dbcd5eaaf1c1fda5aefa5819cb73b98a98
Kobe <243079596+k0bes@users.noreply.github.com>
2026-04-22 18:49:09 -0400

    Add README.md

--- src/boop/render/render.h
+++ src/boop/render/render.h
@@ -1,12 +1,14 @@
 #pragma once
 #include <boop/window/window.h>
 #include <boop/color/color.h>
+#include <math.h>
 class Renderer
 {
 private:
     SDL_Renderer* ren;
 public:
     Renderer(Window window);
+
     void rect(float x, float y, float w, float h, Color color) {
         SDL_FRect rect = { x, y, w, h };
         SDL_SetRenderDrawColor(ren, color.getRed(),color.getGreen(),color.getBlue(),color.getAlpha());
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,14 @@
 #include <boop/window/window.h>
 #include <boop/render/render.h>
-#include <boop/texture/texture.h>
+
+
 
 int main() {
-    Window window = Window("Example Window",1280,720);
+    Window window = Window("text.",1280,720);
     Renderer render = Renderer(window);
-    Texture texture = Texture(window, "image.png");
-    
+ 
     while (window.isOpen()) {
         window.start();
-
-        texture.render(0,0,693,199);
-
         window.end();
     }
 }
